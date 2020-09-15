cbfdd564623b6490c4d15780c64f36fa955bd8d0
code-diggers-369 <codediggers007@gmail.com>
2020-09-15 18:51:28 +0530

    changes to upload on npm

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 const ytdl = require("./index");
 
 ytdl(
-  "https://www.youtube.com/playlist?list=PLvN7nvnjkvpSdo9RMWB1vhONvo2NVn2e7",
-  {
-    quality: "highest",
-  }
+  "https://www.youtube.com/playlist?list=PLvN7nvnjkvpSdo9RMWB1vhONvo2NVn2e7"
 );
